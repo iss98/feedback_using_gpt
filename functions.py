f9c721f30453f9080c626a029059a842406186c0
iss9802 <iss9802@snu.ac.kr>
2023-05-25 18:26:34 +0900

    commit16

--- functions.py
+++ functions.py
@@ -15,6 +15,4 @@ def generate_csv(item, problem, feedback, attempt):
 
 def download_results(item, problem, feedback, attempt):
     csv = generate_csv(item, problem, feedback, attempt)
-    b64 = base64.b64encode(csv.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="quiz_results.csv">다운로드</a>'
-    st.markdown(href, unsafe_allow_html=True)
+    st.download_button("Press to Download", csv, "file.csv", "text/csv", key='download-csv')
