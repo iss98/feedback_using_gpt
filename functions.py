3d71fa80eee7ed411f2b9618ef4bba6fe5976986
iss9802 <iss9802@snu.ac.kr>
2023-05-25 17:57:48 +0900

    commit5

--- functions.py
+++ functions.py
@@ -1,18 +1,20 @@
 import pandas as pd
-import streamlit as st
 import base64
+import streamlit as st
 
 def generate_csv(item, problem, feedback, attempt):
-    data = {"item" : item,
-            "problem" : problem,
-            "feedback" : feedback,
-            "attempt" : attempt}
+    data = {
+        "Item": item,
+        "Problem": problem,
+        "Feedback": feedback,
+        "Attempt": attempt
+    }
     df = pd.DataFrame(data)
     csv_data = df.to_csv(index=False)
     return csv_data
 
 def download_results(item, problem, feedback, attempt):
-    csv = generate_csv(item, problem, feedback, attempt) 
-    b64 = base64.b64encode(csv.encode()).decode() 
-    href = f'<a href="data:file/csv;base64,{b64}" download="quiz_results.csv">다운로드</a>'  
+    csv = generate_csv(item, problem, feedback, attempt)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="quiz_results.csv">다운로드</a>'
     st.markdown(href, unsafe_allow_html=True)
