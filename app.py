cd72a97348b1756e4d7fd6dfcc88e6cb058733ac
iss9802 <iss9802@snu.ac.kr>
2023-06-15 19:04:10 +0900

    fianlfix2

--- app.py
+++ app.py
@@ -38,9 +38,10 @@ st.write("1. 이름과 번호를 페이지의 가장 위에 입력해주세요")
 st.write("2. 문제를 읽고 풀이칸에 자신의 풀이를 입력해주세요")
 st.write("3. 피드백 받기 버튼을 눌러서 인공지능의 피드백을 읽어보세요")
 st.write("4. 피드백을 바탕으로 문제를 다시 풀고 싶은 경우, 풀이칸에 다시 풀이를 적고 피드백 받기 버튼을 누르는 과정을 반복하세요")
+
 st.divider()
 st.header("1번")
-st.markdown("다음 일차방정식을 푸시오. $0.3x + 2 = 0.1x + 0.8$")
+st.markdown("다음 일차방정식을 푸시오 $2x + 13 = -3x -12$")
 response = st.text_input("풀이 : ", key = "input1")
 
 if st.button("피드백 받기", key = "1"):
@@ -55,16 +56,14 @@ if st.button("피드백 받기", key = "1"):
     st.write(fb)
     st.write(f"시도회수 {st.session_state['item_1a']}")
     
-    
 else : st.text("문제를 풀고 피드백 받기를 눌러보세요!")
 
 st.divider()
-
 st.header("2번")
-st.markdown("다음 일차방정식을 푸시오 $2x + 13 = -3x -12$")
+st.markdown("다음 일차방정식을 푸시오. $0.3x + 2 = 0.1x + 0.8$")
 response = st.text_input("풀이 : ", key = "input2")
 
-if st.button("피드백 받기", key = "2"):
+if st.button("피드백 받기", key = "1"):
     st.session_state['item_2a'] +=1
     response = process_string(response)
     fb = generate_fb(prompt2, response)
@@ -74,13 +73,14 @@ if st.button("피드백 받기", key = "2"):
     st.session_state["attempt"].append(st.session_state['item_2a'])
     st.subheader(":robot_face: : GPT의 피드백")
     st.write(fb)
-    st.write(f"시도회수 {st.session_state['item_2a']}")
+    st.write(f"시도회수 {st.session_state['item_1a']}")
+    
     
 else : st.text("문제를 풀고 피드백 받기를 눌러보세요!")
 
 st.divider()
 st.header("3번")
-st.markdown("하민이와 민혁이는 종이를 접어 장미꽃을 만들기로 하였다. 하민이는 10분에 장미꽃 4송이를, 민혁이는 12분에 장미꽃 6송이를 만들 수 있다. 하민이와 민혁이가 집에서 각각 장미꽃 16송이와 12송이를 미리 만들어 와서 동시에 장미꽃을 만들기 시작하였다. 두 사람의 장미꽃의 개수가 같아지는 것은 장미꽃을 만들기 시작한 지 몇 분 후인지 구하고, 그 풀이 과정을 쓰시오.")
+st.markdown("연속한 두 수의 합이 $59$일 때, 큰 수를 구하세요.")
 response = st.text_input("풀이 : ", key = "input3")
 
 if st.button("피드백 받기", key = "3"):
@@ -98,7 +98,6 @@ if st.button("피드백 받기", key = "3"):
 else : st.text("문제를 풀고 피드백 받기를 눌러보세요!")
 
 st.divider()
-
 st.header("4번")
 st.markdown("연속하는 세 자연수의 합이 $30$일 때, 이 세 자연수 중에서 가장 큰 자연수의 값을 자세한 설명과 함께 구하시오.")
 response = st.text_input("풀이 : ", key = "input4")
@@ -120,7 +119,7 @@ else : st.text("문제를 풀고 피드백 받기를 눌러보세요!")
 st.divider()
 
 st.header("5번")
-st.markdown("연속한 두 수의 합이 $59$일 때, 큰 수를 구하세요.")
+st.markdown("하민이와 민혁이는 종이를 접어 장미꽃을 만들기로 하였다. 하민이는 10분에 장미꽃 4송이를, 민혁이는 12분에 장미꽃 6송이를 만들 수 있다. 하민이와 민혁이가 집에서 각각 장미꽃 16송이와 12송이를 미리 만들어 와서 동시에 장미꽃을 만들기 시작하였다. 두 사람의 장미꽃의 개수가 같아지는 것은 장미꽃을 만들기 시작한 지 몇 분 후인지 구하고, 그 풀이 과정을 쓰시오.")
 response = st.text_input("풀이 : ", key = "input5")
 
 if st.button("피드백 받기", key = "5"):
@@ -138,6 +137,7 @@ if st.button("피드백 받기", key = "5"):
 else : st.text("문제를 풀고 피드백 받기를 눌러보세요!")
 
 st.divider()
+
 
 if st.button("결과 다운로드받기"):
     df = generate_df(st.session_state["item"], st.session_state["problem"], st.session_state["feedback"], st.session_state["attempt"])
